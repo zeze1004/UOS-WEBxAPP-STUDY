2a497711ab07f70dd5d4c6e2f0ec1118b88f7d5a
zeze1004 <thwjd2717@naver.com>
2020-08-02 20:54:30 +0900

    gitignore 먼저 커밋하기..^^

---
+++ Index.js
@@ -0,0 +1,24 @@
+// 모듈 선언
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const keys = require('./keys.json')
+
+// express
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+// 이미지 등 정적파일 올리는 폴더
+app.use(express.static('views'))
+
+// port
+const PORT = keys.PORT
+
+// log
+app.use(morgan('dev'))
+
+// f12 소스코드 이쁘게 보여주기
+app.locals.pretty = true;
+
+// db 세팅
+mongoose.connect
